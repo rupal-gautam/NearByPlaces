84fb4556d1bcaf940b6d3d94c9d09f6b3a4aebf3
Rupal Gautam <rgautam@expediagroup.com>
2024-03-17 12:30:21 +0530

    Initial Commit

---
+++ Nearby/NearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyApp.swift
+//  Nearby
+//
+//  Created by Rupal Gautam on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct NearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
